d298d279847743f79f7442992e765f93f3d4d7bf
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-10-08 12:57:15 +0200

    feat: add memes route with CRUD operations

---
+++ routes/memes.mjs
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import { checkSchema, validationResult, matchedData } from "express-validator";
+import { Meme } from "../mongoose/schemas/Meme.mjs";
+import { createMemesValidationSchema } from "../utils/validationSchema.mjs";
+
+const memeRouter = Router();
+
+memeRouter.get("/", async (req, res) => {
+    try {
+        const memes = await Meme.find();
+
+        res.json(memes);
+    } catch (error) {
+        console.error("Error fetching memes:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+memeRouter.post("/", checkSchema(createMemesValidationSchema), async (req, res) => {
+    const results = validationResult(req);
+
+    if (!results.isEmpty()) {
+        return res.status(400).json({ errors: results.array() });
+    }
+
+    const data = matchedData(req);
+    const newMeme = new Meme(data);
+
+    try {
+        const savedMeme = await newMeme.save();
+
+        return res.status(201).json(savedMeme);
+    } catch (error) {
+        console.error("Error creating meme:", error);
+        res.status(400).json({ error: "Bad Request" });
+    }
+});
+
+export default memeRouter;
