b04a371c12cd89a61a295c3d1b581c02e215646d
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-10-08 12:57:02 +0200

    refactor: update Mongoose schemas formatting

--- mongoose/schemas/Calendar.mjs
+++ mongoose/schemas/Calendar.mjs
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
 const memeSchema = new mongoose.Schema({
-  memeIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Meme" }],
-  roomId: { type: mongoose.Schema.Types.ObjectId, ref: "Room", required: true },
+    memeIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Meme" }],
+    roomId: { type: mongoose.Schema.Types.ObjectId, ref: "Room", required: true },
 });
 
 const Calendar = mongoose.model("Calendar", memeSchema);
+
 export default Calendar;
--- mongoose/schemas/User.mjs
+++ mongoose/schemas/User.mjs
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: { type: mongoose.Schema.Types.String, required: true, unique: true },
-  password: { type: mongoose.Schema.Types.String, required: true },
-  createdAt: { type: mongoose.Schema.Types.Date, default: Date.now },
-  avatarUrl: { type: mongoose.Schema.Types.String, default: '' },
-  roomIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Room' }],
-  role: { type: mongoose.Schema.Types.String, enum: ['user','owner', 'admin'], default: 'user' },
+    username: { type: mongoose.Schema.Types.String, required: true, unique: true },
+    password: { type: mongoose.Schema.Types.String, required: true },
+    createdAt: { type: mongoose.Schema.Types.Date, default: Date.now },
+    avatarUrl: { type: mongoose.Schema.Types.String, default: "" },
+    roomIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Room" }],
+    role: { type: mongoose.Schema.Types.String, enum: ["user", "owner", "admin"], default: "user" },
 });
 
-export const User = mongoose.model('User', userSchema);
+export const User = mongoose.model("User", userSchema);
